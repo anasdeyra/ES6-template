26eccdf8b6886e4be7f8a369364ad8809b5356e5
anasdeyra <anasdoura607@gmail.com>
2021-11-21 14:19:40 +0100

    added async support

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import express from "express"
+import express from "express";
 
-const app = express()
+const app = express();
 
 app.listen(4000, () => {
-    console.log("server started at http://localhost:4000")
+  console.log("server started at http://localhost:4000");
+});
 
-})
+app.get("/", async (req, res) => {
+  let s = 0;
+  for (var i = 0; i < 9999999; i++) {
+    s = i;
+  }
+  const f = `aaaa ${await s}`;
+  res.status(200).send(f);
+});
